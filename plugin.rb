21d7805a0dd44a3b4077021f28e8e7e74fa8d719
Carissa K <carissa.cks@gmail.com>
2022-12-08 22:20:45 +0700

    Add plugin code

--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
+# name: apple app site association
+# about: Plugin to serve apple-app-site-association in `.well-known` path
 # version: 0.0.1
-# authors: Discourse
-# url: TODO
+# authors: Lexicon
+# url: https://github.com/carissacks/apple-app-site-association-plugin
 # required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
-
 after_initialize do
+    Discourse::Application.routes.append do
+      get ".well-known/apple-app-site-association" => "metadata#app_association_ios", format: false
+    end
 end
